b5902d69e0cd465170c7a33e0aa380514c2f0d15
Logan Stein <ljstein12@gmail.com>
2023-05-07 17:39:58 -0500

    polyfil fixed

--- config-overrides.js
+++ config-overrides.js
@@ -1,6 +1,11 @@
 const { rewireWebpackConfig } = require("react-app-rewired");
-const customConfig = require("./webpack.config.js");
+// const customConfig = require("./webpack.config.js");
 
 module.exports = function override(config, env) {
-  return rewireWebpackConfig(config, env, () => customConfig);
+  config.resolve.fallback = {
+    ...config.resolve.fallback,
+    https: false,
+    http: false,
+  };
+  return config;
 };
