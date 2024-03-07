a6d80e1bff0bf7e9b6f5b106f0ee44a713274121
Logan Stein <ljstein12@gmail.com>
2024-03-07 11:17:58 -0600

    Add ability to dispay btc as sats

--- src/Components/Asset.js
+++ src/Components/Asset.js
@@ -1,13 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Asset(props) {
-  return (
-    <div className="Asset">
-      <h4>{props.assetCode || "..."}</h4>
-      <p>{props.amount || 215}</p>
-      <p>${Number(props.value.toFixed(2)).toLocaleString("en-US")} USD</p>
-    </div>
-  );
+  const [displaySats, setDisplaySats] = useState(true);
+  if (props.assetCode === "BTC" && displaySats) {
+    return (
+      <div className="Asset">
+        <h4>{props.assetCode || "..."}</h4>
+        <p
+          className="clickable"
+          onClick={() => {
+            setDisplaySats(!displaySats);
+          }}
+        >
+          {(props.amount * 100000000).toLocaleString()} sats
+        </p>
+        <p>${Number(props.value.toFixed(2)).toLocaleString("en-US")} USD</p>
+      </div>
+    );
+  } else {
+    return (
+      <div className="Asset">
+        <h4>{props.assetCode || "..."}</h4>
+        <p
+          className="clickable"
+          onClick={() => {
+            setDisplaySats(!displaySats);
+          }}
+        >
+          {props.amount || 215}
+        </p>
+        <p>${Number(props.value.toFixed(2)).toLocaleString("en-US")} USD</p>
+      </div>
+    );
+  }
 }
 
 export default Asset;
